0d67a9df94c66e8d7c0afa4e0e5307a3927acc6a
KlausJung011 <nicolas.nsavs@gmail.com>
2026-09-25 09:49:39 -0400

    feat: Cambios en Landing Page y Bind mount en Docker Compos

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from app import create_app
+import glob
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    # Archivos extra que Flask debe vigilar para recargar
+    extra_files = (
+        glob.glob('app/templates/**/*.html', recursive=True) +
+        glob.glob('app/static/**/*.css',     recursive=True) +
+        glob.glob('app/static/**/*.js',      recursive=True)
+    )
+    app.run(host='0.0.0.0', port=5000, debug=True, extra_files=extra_files)
